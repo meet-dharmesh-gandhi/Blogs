fa512a0c26d44d5684a1a5c1dd24ac699928d5cd
GeniusMind9999 <meetgandhi13062005@gmail.com>
2025-08-20 17:29:55 +0530

    redirect to auth website

--- Flutter External Authentication/app/lib/LoginScreen.dart
+++ Flutter External Authentication/app/lib/LoginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +10,33 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
+  Future<void> redirectToWebAuth() async {
+    final Uri uriWithParams = Uri.parse("http://localhost:3000?port=56430");
+    if (!await launchUrl(uriWithParams, mode: LaunchMode.externalApplication)) {
+      print("Failed to launch the url!");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Login Screen!"));
+    return Column(
+      spacing: 30,
+      children: [
+        Text("Login Screen!"),
+        TextButton(
+          onPressed: redirectToWebAuth,
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.white),
+            padding: WidgetStatePropertyAll(EdgeInsets.all(16)),
+          ),
+          child: Text("Login!", style: TextStyle(fontWeight: FontWeight.bold)),
+        ),
+      ],
+    );
   }
 }
